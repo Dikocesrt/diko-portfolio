ec2e467091b249a6e4ea56c92f2e52e4fa0de769
Dikocesrt <dikocesrt@gmail.com>
2025-05-03 00:51:57 +0700

    finished show list experiences and projects controllers

---
+++ src/controllers/experience.js
@@ -0,0 +1,35 @@
+const { Experience } = require("../models/association");
+const getURL = require("../helper/getCloudinary");
+const sequelize = require("../configs/database");
+
+const showExperiences = async (req, res) => {
+    try {
+        const experiences = await Experience.findAll({
+            order: [["created_at", "ASC"]],
+            include: [
+                {
+                    model: ExperienceCategory,
+                    as: "category",
+                    attributes: ["name"],
+                },
+            ],
+        });
+
+        const plainExperiences = experiences.map((experience) =>
+            experience.get({ plain: true })
+        );
+
+        plainExperiences.map((experience) => {
+            if (experience.image) {
+                experience.image = getURL(experience.image, 400, 200);
+            }
+        });
+
+        res.render("experiences/list", {
+            isExperience: true,
+            experiences: plainExperiences,
+        });
+    } catch (error) {
+        console.log("SHOW LIST ERROR => " + error);
+    }
+};
---
+++ src/controllers/project.js
@@ -0,0 +1,68 @@
+const {
+    Project,
+    ProjectCategory,
+    Experience,
+} = require("../models/association");
+const getURL = require("../helper/getCloudinary");
+const sequelize = require("../configs/database");
+
+const showProjects = async (req, res) => {
+    try {
+        let projects, isProject, experience;
+
+        if (req.params.id) {
+            experience = await Experience.findOne({
+                where: {
+                    id: req.params.id,
+                },
+            });
+
+            projects = await Project.findAll({
+                order: [["created_at", "ASC"]],
+                where: {
+                    experienceId: req.params.id,
+                },
+                include: [
+                    {
+                        model: ProjectCategory,
+                        as: "category",
+                        attributes: ["name"],
+                    },
+                ],
+            });
+
+            isProject = false;
+        } else {
+            projects = await Project.findAll({
+                order: [["created_at", "ASC"]],
+                include: [
+                    {
+                        model: ProjectCategory,
+                        as: "category",
+                        attributes: ["name"],
+                    },
+                ],
+            });
+
+            isProject = true;
+        }
+
+        const plainProjects = projects.map((project) =>
+            project.get({ plain: true })
+        );
+
+        plainProjects.map((project) => {
+            if (project.image) {
+                project.image = getURL(project.image, 400, 200);
+            }
+        });
+
+        res.render("projects/list", {
+            projects: plainProjects,
+            isProject,
+            experience,
+        });
+    } catch (error) {
+        console.log("SHOW PROJECTS ERROR => " + error);
+    }
+};
